1ca3111bafa81a92ca488c39ccf09b71e1935b54
danroberts728 <danroberts728@gmail.com>
2017-03-07 11:38:15 -0600

    Added Problem 70

---
+++ ProjectEulerAnswers/Controllers/Problem70Controller.cs
@@ -0,0 +1,84 @@
+﻿using ProjectEulerAnswers.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectEulerAnswers.Utility;
+
+namespace ProjectEulerAnswers.Controllers
+{
+    public class Problem70Controller : Controller
+    {
+        // GET: Problem70
+        public ActionResult Index()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            ProjectEulerAnswer answer = new ProjectEulerAnswer();
+
+            List<int> primes = PrimesUtility.Generate(5000).ToList();
+            primes.RemoveAll(x => x <= 2000);
+            long bestN = 1;
+            long bestPhi = 1;
+            double bestRatio = double.PositiveInfinity;
+            for(int i = 0; i < primes.Count; i++)
+            {
+                for(int j = 0; j < primes.Count; j++)
+                {
+                    long n = primes[i] * primes[j];
+                    if(n > 10000000)
+                    {
+                        break;
+                    }
+
+                    long phi = (primes[i] - 1) * (primes[j] - 1);
+                    double ratio = ((double)n) / phi;
+
+                    if(isPerm(phi, n) && bestRatio > ratio)
+                    {
+                        bestN = n;
+                        bestPhi = phi;
+                        bestRatio = ratio;
+                    }
+                }
+            }
+            answer.Answer = bestN.ToString(); ;
+
+            stopwatch.Stop();
+            stopwatch.Reset();
+
+            return View(answer);
+        }
+
+        private bool isPerm(long m, long n)
+        {
+            int[] arr = new int[10];
+
+            long temp = n;
+            while (temp > 0)
+            {
+                arr[temp % 10]++;
+                temp /= 10;
+            }
+
+            temp = m;
+            while (temp > 0)
+            {
+                arr[temp % 10]--;
+                temp /= 10;
+            }
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (arr[i] != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ ProjectEulerAnswers/Models/ProjectEulerAnswer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectEulerAnswers.Models
+{
+    public class ProjectEulerAnswer
+    {
+        public string Answer { get; set; }
+        public double ElapsedMs { get; set; }
+
+        public override string ToString()
+        {
+            return Answer.ToString();
+        }
+    }
+}
